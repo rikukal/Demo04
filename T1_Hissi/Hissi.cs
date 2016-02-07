b4a149e5ad2cb1f105ad8fa2b6acf3c7d44949f9
rikukal <h8851@student.jamk.fi>
2016-02-07 14:38:24 +0200

    Valmis T1 Hissi

--- T1_Hissi/Hissi.cs
+++ T1_Hissi/Hissi.cs
@@ -8,13 +8,41 @@ namespace T1_Hissi
 {
     class Hissi
     {
-        //field variables
+        //field variables (tässä annetaan PRIVAATISTI olion ominaisuuksille RAJA-ARVOT
 
-        // arvojen maksimi ja minimiarvojen määritelmät if, (if else) ja else lausekkeita
+        private readonly int maxKerros = 5;
+        private int kerros;
+        
 
-        // property variables kerrotaan arvot ja puretaan get - set - malli
-        public int Floor { get; set; }
+        // property variables kerrotaan arvot ja puretaan get - set - mallin avulla
+        public int Kerros 
+        {
 
+            get
+            {
+                return kerros;
+            }
 
+            set // arvojen maksimi ja minimiarvojen määritelmät if, (if else) ja else lausekkeita ilmoitetaan set-mallilla
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Et voi kulkea hissillä alemmas");
+                        kerros = 1;
+                }
+                else if (value > 5)
+                {
+                    Console.WriteLine("Et voi kulkea hissillä ylemmäs");
+                        kerros = 5;
+                }
+                else
+                {
+                    kerros = value;
+                }
+            }
+
+        }
+
+        
     }
 }
--- T1_Hissi/Program.cs
+++ T1_Hissi/Program.cs
@@ -11,7 +11,9 @@ namespace T1_Hissi
         static void Main(string[] args)
         {
             //create an elevator object
-
+            Hissi hissi = new Hissi();
+            hissi.Kerros = -1;       // Tämä arvo näkyy tulosteessa - olettaen, että se on raja-arvojen sisällä -- muutoin tulosteksti asettaa lähimmän raja-arvon
+            Console.WriteLine("Olet kerroksessa {0}",hissi.Kerros); // tulostettuun tekstiin arvo pitää ilmaista muotoa: luokan olion nimi(hissi[pienellä].Ominaisuus (Kerros[ISOLLA ALKUKIRJAIMELLA]) 
         }
     }
 }
